a0500ec268fb753ce23e99b399b4b997dfadac12
Sid-321 <sidhantgogreen@gmail.com>
2017-03-21 10:49:05 +0530

    first commit

---
+++ app/src/main/res/layout/activity_main.java
@@ -0,0 +1,32 @@
+package info.androidhive.androidsplashscreentimer;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreen extends Activity {
+
+
+    private static int SPLASH_TIME_OUT = 300;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                Intent i = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(i);
+
+                // close this activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+
